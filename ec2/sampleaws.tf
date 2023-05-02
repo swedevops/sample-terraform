d5aeb63123414dab619cdc8fb349d3fab5967b1c
swedev99 <swedevops99@gmail.com>
2023-05-02 23:21:49 +0530

    code dry

--- ec2/sampleaws.tf
+++ ec2/sampleaws.tf
@@ -18,14 +18,16 @@ resource "aws_instance" "frontend" {
   tags = {
     Name = "frontend"
   }
-  resource "aws_route53_record" "frontend" {
-    zone_id = "Z0587270PBVKKHW0FPNL"
+}
+ resource "aws_route53_record" "frontend" {
+   zone_id = Z0587270PBVKKHW0FPNL
     name    = "frontend-dev.swedev99.online"
     type    = "A"
-    ttl     = "30"
+    ttl     = 30
     records = [aws_instance.fronend.private_ip]
-  }
-}
+ }
+
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
