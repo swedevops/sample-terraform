1f23ffacee71a3040ebdf391eb6c4608f4efb4a7
swedev99 <swedevops99@gmail.com>
2023-05-02 22:54:58 +0530

    code dry

--- ec2/sampleaws.tf
+++ ec2/sampleaws.tf
@@ -6,3 +6,75 @@ resource "aws_instance" "HelloWorld" {
     Name = "HelloWorld"
   }
 }
+resource "aws_instance" "frontend" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_instance" "mongodb" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_instance" "redis" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "redis"
+  }
+}
+resource "aws_instance" "user" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
+resource "aws_instance" "cart" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+resource "aws_instance" "catlogue" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+resource "aws_instance" "mysql" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+resource "aws_instance" "payment" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "payment"
+  }
+}
