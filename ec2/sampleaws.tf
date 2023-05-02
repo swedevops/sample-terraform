c22dd98ab5b14d3911b1daceed1dd68cb62a07da
swedev99 <swedevops99@gmail.com>
2023-05-02 22:49:13 +0530

    code dry

--- ec2/sampleaws.tf
+++ ec2/sampleaws.tf
@@ -1,16 +1,8 @@
-resource "aws_instance" "centos" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+resource "aws_instance" "HelloWorld" {
+  ami           = ami-0b5a2b5b8f2be4ec2
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
-}
-
-resource "aws_route53_record" "helloworld" {
-  zone_id = "Z0587270PBVKKHW0FPNL"
-  name    = "HelloWorld"
-  type    = "A"
-  ttl     = 30
-  records = [aws_eip.lb.public_ip]
 }
