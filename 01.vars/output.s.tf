4f4a238e3b288032b97bae3bc0b6639279438cbb
swedev99 <swedevops99@gmail.com>
2023-05-03 20:14:39 +0530

    code dry

--- 01.vars/output.s.tf
+++ 01.vars/output.s.tf
@@ -1,4 +1,18 @@
 output "sample_string" {
   value = var.sample_string
 }
-
+outpu "sample_string1" {
+  value = "value of sample_string = ${var.sample_string}"
+}
+output "sample_number"{
+  value = var.sample_number
+}
+output "sample_list"{
+  value = var.sample_list[2]
+}
+output "sample_dict"{
+  value=var.sample_list[number1]
+}
+output "sample_dict1"{
+  value=var.sample_list[string]
+}
--- 01.vars/vars.tf
+++ 01.vars/vars.tf
@@ -1,3 +1,24 @@
 variable  "sample_string" {
 default = "hello world"
 }
+variable "sample_number"{
+  default = 199
+}
+variable "sample_boolean"{
+  default = true
+}
+variable "sample_list"{
+  default = [
+  "swethu",
+  99,
+  true,
+  100]
+}
+variable "sample_dict"{
+  defaault = {
+    number1 = 200
+    string = "swetha reddy"
+    number2 ="300"
+    boolean ="true"
+  }
+}
