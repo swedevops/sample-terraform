216adc01d81c860a01698873400236008d49d145
swedev99 <swedevops99@gmail.com>
2023-05-03 13:51:59 +0530

    code dry

--- ec2/sample-security.tf
+++ ec2/sample-security.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "centos" {
+data "aws_ami" "www" {
   owners = ["973714476881"]
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
@@ -13,14 +13,14 @@ resource "aws_instance" "swethu" {
 }
 
   output "swethu" {
-    value = aws_instance.HelloWorld.public_ip
+    value = aws_instance.swethu.public_ip
   }
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z0587270PBVKKHW0FPNL"
-  name    = "swethu-dev.swedev99.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.swethu.private_ip]
-}
+#resource "aws_route53_record" "swethu" {
+ # zone_id = "Z0587270PBVKKHW0FPNL"
+  #name    = "swethu-dev.swedev99.online"
+ # type    = "A"
+ # ttl     = 30
+  #records = [aws_instance.swethu.private_ip]
+#}
 
