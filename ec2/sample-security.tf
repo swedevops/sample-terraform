75fabbf745cd7a54e26f9b8bda8642c471f24de6
swedev99 <swedevops99@gmail.com>
2023-05-03 13:45:08 +0530

    code dry

--- ec2/sample-security.tf
+++ ec2/sample-security.tf
@@ -11,15 +11,15 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
+  output "HelloWorld" {
+    value = aws_instance.HelloWorld.public_ip
+  }
+
 resource "aws_route53_record" "frontend" {
   zone_id = "Z0587270PBVKKHW0FPNL"
   name    = "frontend-dev.swedev99.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
-}
-resource "aws_security_group" "frontend" {
-  name        = "test"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.0e060e06fe63631e4
 }
