08af123c8a80bf7a2e44c19db2537b0686ba6e00
swedev99 <swedevops99@gmail.com>
2023-05-03 18:33:43 +0530

    code dry

--- 01.vars/output.s.tf
+++ 01.vars/output.s.tf
@@ -5,3 +5,9 @@ output "sample_string1" {
   value ="value.sample_string1 = ${var.sample_string}"
 
 }
+output "sample_number"{
+  value = var.sample_number
+}
+output "sample_boolean" {
+  value = var.sample_boolean
+}
--- 01.vars/vars.tf
+++ 01.vars/vars.tf
@@ -1,3 +1,9 @@
 variable "sample_string" {
   default = "hello"
 }
+variable "sample_number" {
+  default = 99
+}
+variable "sample_boolean" {
+  default = true
+}
