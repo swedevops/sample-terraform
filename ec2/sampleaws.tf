4e6299e23d540b52ff4f6c08ae0c14dbf501c03e
swedev99 <swedevops99@gmail.com>
2023-05-03 13:40:39 +0530

    code dry

--- ec2/sampleaws.tf
+++ ec2/sampleaws.tf
@@ -11,6 +11,10 @@ resource "aws_instance" "HelloWorld" {
     Name = "HelloWorld"
   }
 }
+  output "HelloWorld" {
+    value = aws_instance.HelloWorld.public_ip
+  }
+
 resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
