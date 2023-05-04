2b98a9ccbaaf82e9b63bd29fd5d8f2544ad3f7cf
swedev99 <swedevops99@gmail.com>
2023-05-04 19:36:09 +0530

    code dry

--- Data-sources/securitymain.tf
+++ Data-sources/securitymain.tf
@@ -51,7 +51,7 @@ variable "components" {
   }
   }
 resource "aws_instance" "instance" {
-  for-each      = var.components
+  for_each      = var.components
   ami           = data.aws_ami.centos.image_id
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.test.id]
