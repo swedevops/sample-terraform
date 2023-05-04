745ddd0de84c7943a7bdb9eb0525a489daf767e3
swedev99 <swedevops99@gmail.com>
2023-05-04 08:46:04 +0530

    code dry

--- Data-sources/securitymain.tf
+++ Data-sources/securitymain.tf
@@ -2,4 +2,5 @@ data "aws_security_group" "selected" {
   name = "test"
 }
 output "security_group_id" {
-  value = "data.aws_security_group.selected.id"
+  value = "data.aws_security_group.selected"
+}
