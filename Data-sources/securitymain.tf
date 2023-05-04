4cf1fb0474acba9aa7b16113146776e5b1155d27
swedev99 <swedevops99@gmail.com>
2023-05-04 09:30:53 +0530

    code dry

--- Data-sources/securitymain.tf
+++ Data-sources/securitymain.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "www" {
+data "aws_ami" "centos" {
   owners = ["973714476881"]
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
@@ -6,15 +6,13 @@ data "aws_ami" "www" {
 data "aws_security_group" "test" {
   name = "test"
 }
-variable "instance_type" {
-  default ="t3.micro"
-}
-resource "aws_instance" "swethu" {
+resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "var.instance_type"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.test.id]
+
   tags = {
-    Name = "swethu"
+    Name = "frontend"
   }
 }
 
