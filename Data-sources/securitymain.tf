534813f02288e445cda38c47f7fd147b9d994ae3
swedev99 <swedevops99@gmail.com>
2023-05-04 08:45:12 +0530

    code dry

---
+++ Data-sources/securitymain.tf
@@ -0,0 +1,5 @@
+data "aws_security_group" "selected" {
+  name = "test"
+}
+output "security_group_id" {
+  value = "data.aws_security_group.selected.id"
--- ec2/sample-security.tf
+++ ec2/sample-security.tf
@@ -1,5 +1,26 @@
-data "aws_security_group" "selected" {
-  name = "test"
+data "aws_ami" "www" {
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
 }
-output  "security_group_id" {
-        value = "data.aws_security_group.selected.id"
+resource "aws_instance" "swethu" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "swethu"
+  }
+}
+
+  output "ami" {
+    value = data.aws_ami.centos.image_id
+  }
+
+#resource "aws_route53_record" "swethu" {
+ # zone_id = "Z0587270PBVKKHW0FPNL"
+  #name    = "swethu-dev.swedev99.online"
+ # type    = "A"
+ # ttl     = 30
+  #records = [aws_instance.swethu.private_ip]
+#}
+
